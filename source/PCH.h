49336d20bd1c00cd9b7d942ef9c91da646965c52
xwheeel <erik.melin09@gmail.com>
2025-10-15 17:30:15 +0200

    added glfw

---
+++ source/PCH.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
+#define GLFW_NATIVE_INCLUDE_NONE
+
+#include <glfw/glfw3.h>
+#include <glfw/glfw3native.h>
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
---
+++ source/main.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+int main() {
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "EDGE", nullptr, nullptr);
+    if (!window) return -1;
+
+    bgfx::PlatformData platformData{};
+    platformData.nwh = glfwGetWin32Window(window);
+    bgfx::setPlatformData(platformData);
+
+    bgfx::Init init;
+    init.type = bgfx::RendererType::Count; // auto-detect
+    bgfx::init(init);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        bgfx::frame();
+    }
+
+    bgfx::shutdown();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
